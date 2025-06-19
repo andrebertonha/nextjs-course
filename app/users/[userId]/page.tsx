61dc7ea9429f7e8054a0a991a503f309f23739d1
andrebmartins585 <142544761+andrebmartins585@users.noreply.github.com>
2025-06-19 20:18:41 -0300

    not found page and loading status

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,8 @@
+import { notFound } from "next/navigation";
+
 async function fetchUser(id: string) {
     const res = await fetch(`https://jsonplaceholder.org/users/${id}`);
+
     if(!res.ok) {
         return null;
     }
@@ -13,12 +16,16 @@ export default async function UserPage({
 }: {
     params: Promise<{userId: string}>
 }) {
-    const { userId } = await params;
-    console.log(userId, '<<<<<<<<<<')
+    const { userId } = await params;    
     const user = await fetchUser(userId);
+
+    if(!user) {
+        notFound();
+    }
+
     return (
         <div>
-            <h1>{user.firstName}</h1>
+            <h1>{user.firstname}</h1>
             <p><strong>Email: </strong> {user.email}</p>
             <p><strong>Phone: </strong> {user.phone}</p>
             <p><strong>Website: </strong> <a href={`https://${user.website}`} target="_blank">{user.website}</a></p>
