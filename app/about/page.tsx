15499642ac02e0f2d11fe6d61d0397831224fd98
andrebmartins585 <142544761+andrebmartins585@users.noreply.github.com>
2025-06-19 13:56:45 -0300

    fetch data from json

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
 export default function About() {
+    console.log('hey is this in server or client?');
     return <div>About page</div>;
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,12 @@
-export default function Contact() {
-    return <div>Contact page</div>;
+export default async function Contact() {    
+    console.log('hey is this in server or client?');
+
+    const response = await fetch('https://jsonplaceholder.org/posts');
+    const posts = await response.json();
+    console.log(posts);
+    return (
+        <div>
+            <button >Click me</button>
+        </div>
+    )
 }
