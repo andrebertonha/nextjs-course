f515370bb6bdcf839830c82e2a9b5a6b2a651610
andrebmartins585 <142544761+andrebmartins585@users.noreply.github.com>
2025-06-21 15:12:56 -0300

    google metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,14 @@
-"use client";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us | Name of website ",
+  description: "A lot of keywords",
+  keywords: "about, company, mission, ai, saas",
+  twitter: {
+    card: "summary_large_image",
+    title: "About Us | Twitter",
+  },
+};
 
 export default function About() {
     console.log('hey is this in server or client?');
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,15 @@
+import { Metadata } from "next";
 import ButtonComponent from "./button";
+
+export const metadata: Metadata = {
+  title: "About Us | Name of website ",
+  description: "A lot of keywords",
+  keywords: "about, company, mission, ai, saas",
+  twitter: {
+    card: "summary_large_image",
+    title: "About Us | Twitter",
+  },
+};
 
 export default async function Contact() {    
     console.log('hey is this in server or client?');
