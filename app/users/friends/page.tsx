0def1f3eb3bcd1304eb725f4ee80f58ac656d63a
andrebmartins585 <142544761+andrebmartins585@users.noreply.github.com>
2025-06-20 22:02:34 -0300

    sent message in users/friends from api/hello

--- app/users/friends/page.tsx
+++ app/users/friends/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+
+
+async function makePostRequest() {
+    const res = await fetch(`/api/hello`, {
+        method: "POST",
+        headers: {
+            "Context-Type": "application/json"
+        },
+        body: JSON.stringify({ name: "Andre" }),
+    });
+
+    const data = await res.json();
+    return { data };
+}
+
 export default function Friends() {
-    return <h1> hey friends</h1>
+    const [message, setMessage] = useState("");
+    const onClick = async () => {
+        const {data} = await makePostRequest();
+        setMessage(data.message)
+    }
+    return <h1> hey friends, {message} <button onClick={onClick}>Click Here</button></h1>
 }
